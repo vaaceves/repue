3c9ede5d23be0910ed5a6d89ce326e567b44a582
Alexis Aceves <va.aceves.v@gmail.com>
2017-10-25 02:29:34 -0500

    BASE_DIR Changed

--- repue/settings.py
+++ repue/settings.py
@@ -16,7 +16,8 @@ from decouple import Csv, config
 from unipath import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
